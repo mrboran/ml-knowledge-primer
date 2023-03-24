d9d63e8a4c3c007518d030e2e727489059d0dac0
Madhur <madhur.boran@gmail.com>
2023-03-24 19:28:01 +0100

    Create base.py

---
+++ src/base.py
@@ -0,0 +1,10 @@
+class Base:
+  """Base class for ML Knowledge Primer"""
+    def __init__(self):
+        pass
+  
+    def __str__(self):
+        return f'This class creates the base class for implementing the Machine Learning Knowledge Primer.'
+
+    def __repr__(self):
+        return f'Base Class'
