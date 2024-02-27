c79fc15706a305b0880e940bb6635d034390a2b4
mrboran <madhur.boran@gmail.com>
2024-02-27 23:00:07 +0100

    debug: update config

--- docs/ml-knowledge-primer/docusaurus.config.js
+++ docs/ml-knowledge-primer/docusaurus.config.js
@@ -16,9 +16,10 @@ const config = {
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'mrboran', // Usually your GitHub org/user name.
   projectName: 'ml-knowledge-primer', // Usually your repo name.
-  
+  deploymentBranch: 'main',
+
   // Set the production url of your site here
-  url: 'https://{organizationName}.github.com',
+  url: 'https://mrboran.github.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/<projectName>/',
